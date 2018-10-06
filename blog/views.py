89a66fb12ced57ceb33938ce94934eb8c1a81a6c
soup <soup1234567@gmail.com>
2018-10-06 22:03:37 +0900

    add remove_func, search_func

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,24 @@ from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
+from google.auth import app_engine
+from google.cloud import bigquery
+from google.cloud.bigquery import Dataset
+
+import httplib2
+from oauth2client.contrib import gce
+
 def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     qs = Post.objects.all()
     qs = qs.filter(published_date__lte=timezone.now())
     qs = qs.order_by('published_date')
+
+    q = request.GET.get('q', '') # GET request의 인자중에 q 값이 있으면 가져오고, 없으면 빈 문자열 넣기
+    if q: # q가 있으면
+        qs = qs.filter(title__icontains=q) # 제목에 q가 포함되어 있는 레코드만 필터링
         
-    return render(request, 'blog/post_list.html', {'post_list':qs})
+    return render(request, 'blog/post_list.html', {'post_list':qs, 'q' : q,})
     #modelname_function.html
 
 def post_detail(request, pk):
@@ -22,6 +33,23 @@ def post_detail(request, pk):
     #    post = Post.objects.get(pk=pk)
     #except Post.DoesNotExist:
     #   raise Http404 # Page Not Found
+    
+    credentials = gce.AppAssertionCredentials(scope='https://www.googleapis.com/auth/devstorage.read_write')
+    http = credentials.authorize(httplib2.Http())
+    
+    client = bigquery.Client()
+  
+    # Perform a query.
+    QUERY = (
+        'SELECT spc_common, zipcode FROM `bigquery-public-data.new_york_trees.tree_census_2015` '
+        'WHERE health = "Good" '
+        'LIMIT 10')
+    
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+        
+    for row in rows:
+        print(row.spc_common)
 
     return render(request, 'blog/post_detail.html', {'post':post})
 
@@ -64,6 +92,12 @@ def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
